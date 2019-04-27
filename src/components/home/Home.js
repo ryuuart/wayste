4cf93e6edff1aefc525bb004aa5ffe0a26a0d7f1
Ivan <ijaimes2872@gmail.com>
2019-04-27 14:45:02 -0500

    commit

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -26,6 +26,20 @@ export default class Home extends Component {
 		});
 	}
 	
+	async function quickstart() {
+		// Imports the Google Cloud client library
+		const vision = require('@google-cloud/vision');
+	  
+		// Creates a client
+		const client = new vision.ImageAnnotatorClient();
+	  
+		// Performs label detection on the image file
+		const [result] = await client.labelDetection('./resources/wakeupcat.jpg');
+		const labels = result.labelAnnotations;
+		console.log('Labels:');
+		labels.forEach(label => console.log(label.description));
+	  }
+
 
 	render(){
 
