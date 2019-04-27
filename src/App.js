a1e8b2a250c9fec6621b5cbc5c6457c5b4fa2bdf
Long Nguyen <18nguyenl@gmail.com>
2019-04-27 15:53:35 -0500

    implement hompage design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Wayste</h1>
+      <h2>Can I Throw This Away?</h2>
+      <h3>Choose a button or upload a photo and see</h3>
+      <section>
+        <div className="button">batteries</div>
+        <div className="button">printers</div>
+        <div className="button">cables</div>
+        <div className="button">upload</div>
+      </section>
+      <img src="" alt=""/>
+    </main>
   );
 }
 
